59768b8e17a4a2af4b0afe39eedd62a71a58ca2a
Revanth-sai-2006 <2300031900@kluniversity.in>
2025-08-13 23:10:58 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://3.104.119.6:9090/springapp1';
+const BASE_URL = 'http://54.89.234.218:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
